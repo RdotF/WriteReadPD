e53d953569d980965e05d366fe71934880559f82
RdotF <tsarev02101977@gmail.com>
2023-04-26 19:15:32 +0300

    start: empty main

---
+++ WriteReadPD/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace PD1
+{   
+    class Program
+    {
+        
+        static void Main()
+        {
+            
+        }
+    }
+}
